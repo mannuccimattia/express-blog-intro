6518c7c988c7714b9880401b9bf7bbd678a7b2c2
mannuccimattia <mannuccimattia@gmail.com>
2025-04-16 15:23:01 +0200

    [UPD] Done posts response at /bacheca

--- app.js
+++ app.js
@@ -6,6 +6,43 @@ app.get("/", (req, res) => {
   res.send("Server del mio blog")
 })
 
+const posts = [
+  {
+    title: "Gita al mare",
+    content: "Una giornata di relax al mare",
+    img: "imgs/gita.jpg",
+    tags: ["mare", "relax", "amici"],
+  },
+  {
+    title: "Compleanno",
+    content: "Il mio 30esimo compleanno",
+    img: "imgs/compleanno.jpg",
+    tags: ["festa", "amici", "cena"],
+  },
+  {
+    title: "Matrimonio",
+    content: "Matrimonio di mio fratello",
+    img: "imgs/matrimonio.jpg",
+    tags: ["festa", "famiglia", "ricordi-importanti"],
+  },
+  {
+    title: "Laurea",
+    content: "Consegna della mia laurea",
+    img: "imgs/laurea.jpg",
+    tags: ["famiglia", "carriera", "ricordi-importanti"],
+  },
+  {
+    title: "Pippo",
+    content: "Abbiamo preso un cane!",
+    img: "imgs/doge.jpg",
+    tags: ["famiglia", "animali", "ricordi-importanti"],
+  },
+];
+
+app.get("/bacheca", (req, res) => {
+  res.json(posts)
+})
+
 app.listen(port, () => {
   console.log(`Server in ascolto alla porta ${port}`)
 })
