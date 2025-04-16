9289fe36b8f53f7ba0b8af7a2470097cdddc92db
mannuccimattia <mannuccimattia@gmail.com>
2025-04-16 14:57:06 +0200

    [UPD] Done app response + listen

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server del mio blog")
+})
+
+app.listen(port, () => {
+  console.log(`Server in ascolto alla porta ${port}`)
+})
